f884749e71b12b5183c3f0356b5302959fb04277
Tobias Grah <tobias.grah@cirromed.de>
2025-04-27 00:36:35 +0200

    created project

---
+++ Recipes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Recipes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Recipes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Recipes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
